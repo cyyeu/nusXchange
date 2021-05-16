b0a79b7ef4c08fc8e37cd71a06a3cbfc96b82966
cyyeu <60065592+cyyeu@users.noreply.github.com>
2021-05-16 20:29:37 +0800

    updated psql engine to match django version

--- testdevelopment/testdevelopment/settings.py
+++ testdevelopment/testdevelopment/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'testdevelopment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'user_data',
         'USER': 'postgres',
         'PASSWORD': '12345',
