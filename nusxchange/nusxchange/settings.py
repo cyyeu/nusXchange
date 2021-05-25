a9f1fb5e35ee48646d09275ea994c8edcb5a7dae
cyyeu <60065592+cyyeu@users.noreply.github.com>
2021-05-25 17:07:27 +0800

    setup heroku

--- nusxchange/nusxchange/settings.py
+++ nusxchange/nusxchange/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pp4!u2iw5*x%8h_yv(xho0s3a0qrr!da^)mq_$%(d5hy9p3eey
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nusxchange.herokuapp.com','127.0.0.1']
 
 
 # Application definition
