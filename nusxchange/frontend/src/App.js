a53001e6dc9384d818463e9a5ea408a8205e91b1
jethrokyq <37759922+jethrokyq@users.noreply.github.com>
2021-05-26 20:19:45 +0800

    Update App.js

--- nusxchange/frontend/src/App.js
+++ nusxchange/frontend/src/App.js
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Home, Listing, Profile, Search, Settings, Summarizer } from './views'
+import {Login, Signup, Logout} from './views/auth/';
+import Landing from './Landing'
+import { ThemeProvider } from '@material-ui/core/styles'
+import { Theme } from './Theme'
+
+
 const App = () => {
   return (
     <Router>
