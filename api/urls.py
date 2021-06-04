a6b53d4f0f1c42e8b9a21f14cc6a06fd98d3f236
jethrokyq <37759922+jethrokyq@users.noreply.github.com>
2021-06-04 17:38:32 +0800

    remove session auth and test password reset

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from dj_rest_auth.views import PasswordResetConfirmView
 
 
 urlpatterns = [
+    path('password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('auth/', include('dj_rest_auth.urls')),
 		path('auth/register', include('dj_rest_auth.registration.urls'))
 ]
--- nusxchange/settings.py
+++ nusxchange/settings.py
@@ -187,7 +187,6 @@ REST_FRAMEWORK = {
     'DATETIME_FORMAT': "%m/%d/%Y %I:%M%P",
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication'
     ],
 }
 
